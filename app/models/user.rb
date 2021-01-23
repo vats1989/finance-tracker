0c9d2d1d1232e74b1ba7693c4690081dcca17e12
Vatsal Sevak <vsevak@zendesk.com>
2021-01-22 17:50:57 -0800

    first_name and last_name fields for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,9 @@ class User < ApplicationRecord
   def can_track_stock?(ticker)
     under_stock_limit? && !stock_already_tracked?(ticker)
   end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
 end
